df856b058d932794666e426f9286a8098e922d49
Klimiwe <klimiwe@mail.ru>
2022-07-23 13:24:46 +0400

    Eight task

--- BlockDemo.java
+++ BlockDemo.java
@@ -1,9 +1,9 @@
 public class BlockDemo {
 
     public static void main (String args[]){
-Double i, j, d;
-i = (double) 5;
-j = (double) 10;
+    Double i, j, d;
+    i  = (double) 5;
+    j = (double) 10;
 
 if (i!=0){
     System.out.println("i does not equel zero");
