c3025e6a3bef3ceac3e47459aa8cd18ff26bd064
Klimiwe <klimiwe@mail.ru>
2022-07-26 09:22:16 +0400

    Second chapter without first chapter

--- Example.java
+++
@@ -1,5 +0,0 @@
-class Example {
-    public static void main (String args[]){
-        System.out.println("Java Drives the Web");
-    }
-}
--- ForDemo.java
+++
@@ -1,9 +0,0 @@
-public class ForDemo {
-    public static void main (String args[]){
-        int count;
-        for (count = 0; count<5; count = count+1)
-        System.out.println("This is count " + count);
-        
-        System.out.println("Done");
-    }
-}
