be0b6dabe1853ffbb8ed5e4fb9210f990d51edf5
Klimiwe <klimiwe@mail.ru>
2022-08-01 19:33:39 +0400

    Gues with mystake

--- Guess.java
+++ Guess.java
@@ -1,21 +1,23 @@
 public class Guess {
     public static void main (String args[])
        throws java.io.IOException {
-            char ch, answer = 's';
-            int i = 0;
+            char ch, answer = 'k';
+            int i;
             ch = 'g';
             System.out.println("Отгадай какую букву латинского алфавита я загадал");
             
-            for (i=0; ch!=answer; i++){
+            for (i=1; ch!=answer; i++){
             System.out.println("Возможно это: ");
-
             ch = (char)
             System.in.read();
-         if(ch == answer)
+            
+
+        if (ch != answer)
+         System.out.println("Не верно попробуй еще раз.");}
+
+        
             System.out.println("Молодец ты угадала!!!");
-            else 
-            System.out.println("Не верно, попробуй еще.");
-            }
         }
-    }
+        }
+    
 
